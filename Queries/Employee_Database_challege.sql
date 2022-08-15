68eb09bc704c14072ea03762bb79cbfcccd1869f
Normanfamdamly <104736546+Normanfamdamly@users.noreply.github.com>
2022-08-14 20:04:01 -0400

    Update Employee_Database_challege.sql

--- Queries/Employee_Database_challege.sql
+++ Queries/Employee_Database_challege.sql
@@ -28,3 +28,20 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
+
+-- Deliverable 2 Employees eligable for the Mentorship Program
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
